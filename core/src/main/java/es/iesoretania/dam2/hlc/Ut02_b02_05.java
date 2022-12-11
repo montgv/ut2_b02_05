db4b04711dcb0ea2b0d3b077d28d13dc4e524bb7
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2022-12-11 22:47:37 +0100

    terminado

--- core/src/main/java/es/iesoretania/dam2/hlc/Ut02_b02_05.java
+++ core/src/main/java/es/iesoretania/dam2/hlc/Ut02_b02_05.java
@@ -13,97 +13,79 @@ import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Ut02_b02_05 extends ApplicationAdapter {
-	TiledMap map;
+	TiledMap mapa;
 	OrthographicCamera camera;
 	OrthogonalTiledMapRenderer mapRenderer;
+	
 	private int mapWidthInPixels;
 	private int mapHeightInPixels;
 	private float offsetX, offsetY;
 
-	private BitmapFont texto;
-	private Texture image;
+	private BitmapFont coordenadas;
+	private Texture imagen;
 
-	private TextureRegion parado_frente, parado_derecha, parado_izquierda,parado_espalda;
-	private TextureRegion espalda1, espalda2;
-	private TextureRegion derecha1, derecha2;
-	private TextureRegion izquierda1, izquierda2;
-	private TextureRegion frente1, frente2;
-	boolean pasos;
+	private TextureRegion heroeDelante, heroeDetras, heroeDerecha, heroeIzquierda;
 
 	Sprite heroe;
 	SpriteBatch batch;
 
 	@Override
 	public void create() {
-		//HEROE
+		//Creamos la textura y el texto
 		batch = new SpriteBatch();
-		image = new Texture("UT02_B02_Heroe.png");
-		texto = new BitmapFont();
+		imagen = new Texture(Gdx.files.internal("UT02_B02_Heroe.png"));
+		coordenadas = new BitmapFont();
 
-		parado_frente = new TextureRegion(image, 24, 64, 24, 32);
-		parado_derecha = new TextureRegion(image, 24, 32, 24, 32);
-		parado_izquierda = new TextureRegion(image, 24, 96, 24, 32);
-		parado_espalda = new TextureRegion(image, 24, 0, 24, 32);
+		//Diferentes texturas del heroe
+		heroeDelante = new TextureRegion(imagen, 24, 64, 24, 32);
+		heroeDerecha = new TextureRegion(imagen, 24, 32, 24, 32);
+		heroeIzquierda = new TextureRegion(imagen, 24, 96, 24, 32);
+		heroeDetras = new TextureRegion(imagen, 24, 0, 24, 32);
 
-		espalda1 = new TextureRegion(image, 0, 0, 24, 32);
-		espalda2 = new TextureRegion(image, 0, 45, 24, 32);
-
-		derecha1 = new TextureRegion(image, 32, 0, 24, 32);
-		derecha2 = new TextureRegion(image, 32, 45, 24, 32);
-
-		izquierda1 = new TextureRegion(image, 96, 0, 24, 32);
-		izquierda2 = new TextureRegion(image, 96, 45, 24, 32);
-
-		frente1 = new TextureRegion(image, 64, 0, 24, 32);
-		frente2 = new TextureRegion(image, 64, 45, 24, 32);
-
+		//Creamos el sprite
 		heroe = new Sprite();
-		//heroe.setX(320 - parado_frente.getRegionWidth() / 2.0f);
-		//heroe.setY(240 - parado_frente.getRegionHeight() / 2.0f);
 		heroe.setPosition(320,240);
 		heroe.setSize(24,32);
 
-		//MAPA
-		map = new TmxMapLoader().load("mapa_oretania.tmx");
-		MapProperties properties = map.getProperties();
+		//Creamos el mapa
+		mapa = new TmxMapLoader().load("tiled.tmx");
+		MapProperties properties = mapa.getProperties();
 		int tileWidth = properties.get("tilewidth", Integer.class);
 		int tileHeight = properties.get("tileheight", Integer.class);
 		int mapWidthInTiles = properties.get("width", Integer.class);
 		int mapHeightInTiles = properties.get("height", Integer.class);
 		mapWidthInPixels = mapWidthInTiles * tileWidth;
 		mapHeightInPixels = mapHeightInTiles * tileHeight;
-		mapRenderer = new OrthogonalTiledMapRenderer(map);
+		mapRenderer = new OrthogonalTiledMapRenderer(mapa);
 		camera = new OrthographicCamera();
-		heroe.setRegion(parado_izquierda);
+		heroe.setRegion(heroeDelante);
 		offsetX = 0;
 		offsetY = 0;
 	}
 	@Override
 	public void render() {
-		/*if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) offsetX -= 400 * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) offsetX += 400 * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Input.Keys.UP)) offsetY += 400 * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) offsetY -= 400 * Gdx.graphics.getDeltaTime();*/
 
+		//Indicamos al heroe que tiene que hacer cuando pulsamos alguna de esa tecla
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
 			heroe.translateX(-100 * Gdx.graphics.getDeltaTime());
-			heroe.setRegion(parado_izquierda);
+			heroe.setRegion(heroeIzquierda);
 		}else{
 			if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
 				heroe.translateX(100 * Gdx.graphics.getDeltaTime());
-				heroe.setRegion(parado_derecha);
+				heroe.setRegion(heroeDerecha);
 			}
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
 			heroe.translateY(100 * Gdx.graphics.getDeltaTime());
-			heroe.setRegion(parado_espalda);
+			heroe.setRegion(heroeDetras);
 		}else{
 			if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
 				heroe.translateY(-100 * Gdx.graphics.getDeltaTime());
-				heroe.setRegion(parado_frente);
+				heroe.setRegion(heroeDelante);
 			}
 		}
 
+		//Indicamos al heroe que no se salga de los limites de la pantalla en el eje x y en el eje y
 		if(heroe.getX() > camera.viewportWidth - 100 && offsetX < mapWidthInPixels - camera.viewportWidth){
 			offsetX += 100 * Gdx.graphics.getDeltaTime();
 			heroe.setX(camera.viewportWidth - 100);
@@ -112,8 +94,12 @@ public class Ut02_b02_05 extends ApplicationAdapter {
 			offsetX -= 100 * Gdx.graphics.getDeltaTime();
 			heroe.setX(100);
 		}
-		if(heroe.getX() < 0) heroe.setX(0);
-		if(heroe.getX() > camera.viewportWidth - heroe.getWidth()) heroe.setX(camera.viewportWidth - heroe.getWidth());
+		if(heroe.getX() < 0) {
+			heroe.setX(0);
+		}
+		if(heroe.getX() > camera.viewportWidth - heroe.getWidth()) {
+			heroe.setX(camera.viewportWidth - heroe.getWidth());
+		}
 
 		if(heroe.getY() > camera.viewportHeight - 100 && offsetY < 0){
 			offsetY += 100 * Gdx.graphics.getDeltaTime();
@@ -123,30 +109,47 @@ public class Ut02_b02_05 extends ApplicationAdapter {
 			offsetY -= 100 * Gdx.graphics.getDeltaTime();
 			heroe.setY(100);
 		}
-		if(heroe.getY() < 0) heroe.setY(0);
-		if (heroe.getY() > camera.viewportHeight - heroe.getRegionHeight()) heroe.setY(camera.viewportHeight - heroe.getHeight());
+		if(heroe.getY() < 0) {
+			heroe.setY(0);
+		}
+		if (heroe.getY() > camera.viewportHeight - heroe.getRegionHeight()) {
+			heroe.setY(camera.viewportHeight - heroe.getHeight());
+		}
 
-		if (offsetX < 0) offsetX = 0;
-		if (offsetY > 0) offsetY = 0;
-		if (offsetX > mapWidthInPixels - camera.viewportWidth) offsetX = mapWidthInPixels - camera.viewportWidth;
-		if (offsetY < -mapHeightInPixels + camera.viewportHeight) offsetY = -mapHeightInPixels + camera.viewportHeight;
+		//Indicamos a la camara que no se salga de la pantalla
+		if (offsetX < 0) {
+			offsetX = 0;
+		}
+		if (offsetY > 0) {
+			offsetY = 0;
+		}
+		if (offsetX > mapWidthInPixels - camera.viewportWidth) {
+			offsetX = mapWidthInPixels - camera.viewportWidth;
+		}
+		if (offsetY < -mapHeightInPixels + camera.viewportHeight) {
+			offsetY = -mapHeightInPixels + camera.viewportHeight;
+		}
 
 		camera.position.x = camera.viewportWidth / 2 + offsetX;
 		camera.position.y = mapHeightInPixels - camera.viewportHeight / 2 + offsetY;
 
+		//Diferentes capas que tiene el mapa
 		int[] capasFondo = {0, 1, 2};
 		int[] capasAltas = {3};
 
+		//Se dibujan las diferentes elementos
 		camera.update();
 		mapRenderer.setView(camera);
 		mapRenderer.render(capasFondo);
 
 		batch.begin();
-		//batch.setProjectionMatrix(camera.projection);
-		texto.draw(batch,"(" + offsetX + " , " + offsetY + ")",10,15);
+		coordenadas.draw(batch,"(" + offsetX + " , " + offsetY + ")",10,15);
 		heroe.draw(batch);
 		batch.end();
+		mapRenderer.render(capasAltas);
 	}
+
+	//Este método se usa para informar del tamaño de la pantalla
 	@Override
 	public void resize(int width, int height) {
 		super.resize(width, height);
@@ -155,11 +158,13 @@ public class Ut02_b02_05 extends ApplicationAdapter {
 		camera.position.y = mapHeightInPixels - camera.viewportHeight / 2 + offsetY;
 		camera.update();
 	}
+
+	//Este método se llama paara liberar los recursos utilizados
 	@Override
 	public void dispose() {
 		batch.dispose();
-		image.dispose();
-		map.dispose();
-		texto.dispose();
+		imagen.dispose();
+		mapa.dispose();
+		coordenadas.dispose();
 	}
 }
